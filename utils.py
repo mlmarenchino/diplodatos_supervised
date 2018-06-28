a7e45ec08dc278fc5e237f4a043621af2e3e961d
Matias Marenchino <matias_marenchino@mcafee.com>
2018-06-28 18:39:13 -0300

    Update utils

--- utils.py
+++ utils.py
@@ -1,24 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-def load_heart_dataset(div):
-    # The larger the div the more distributed the samples
-    np.random.seed(1)
-    m = 400 # number of samples
-    N = int(m/div) # number of points per class
-    D = 2 # dimensionality
-    X = np.zeros((m,D)) # data matrix where each row is a single example
-    Y = np.zeros((m,1), dtype='uint8') # labels vector
-
-    for j in range(div):
-        ix = range(N*j,N*(j+1))
-        t = np.linspace(j*np.pi/(div/2),(j+1)*np.pi/(div/2),N) + np.random.randn(N)*0.2
-        X[ix] = np.c_[16*np.sin(t)**3, 13*np.cos(t) - 5*np.cos(2*t) - 2*np.cos(3*t) - np.cos(4*t)]
-        Y[ix] = j % 2
-
-    return X.T, Y.T
-
 def plot_decision_boundary(model, X, y):
     # Set min and max values and give it some padding
     x_min, x_max = X[0, :].min() - 1, X[0, :].max() + 1
